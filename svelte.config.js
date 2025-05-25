2c40f84338ed7101329b8538f583c23d03e66cbf
Kyruu17 <vasin1609@hotmail.com>
2025-05-25 15:32:18 +0200

    okk

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
  
-const config = {
-    preprocess: preprocess(),
-    compilerOptions: {
-        runes: true
-    },
-    kit: {
-        adapter: adapter()
-    }
+import adapter from '@sveltejs/adapter-netlify';
+export default {
+kit: {
+adapter: adapter({
+edge: false,
+split: false
+})
+}
 };
- 
-export default config;
